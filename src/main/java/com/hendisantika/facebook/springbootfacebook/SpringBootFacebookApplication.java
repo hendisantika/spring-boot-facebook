569adb37954698ce498b4945ad8e56cf6e2aa6ce
Hendi Santika <hendisantika@yahoo.co.id>
2018-12-13 08:17:47 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/facebook/springbootfacebook/SpringBootFacebookApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.facebook.springbootfacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFacebookApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/facebook/springbootfacebook/SpringBootFacebookApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.facebook.springbootfacebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFacebookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
