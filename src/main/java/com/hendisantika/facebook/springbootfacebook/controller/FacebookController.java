5bd14f6f52c3b4735c5c759afb6f2ff83904043d
Hendi Santika <hendisantika@yahoo.co.id>
2018-12-13 08:25:20 +0700

    #4 - Add Facebook Controller

---
+++ src/main/java/com/hendisantika/facebook/springbootfacebook/controller/FacebookController.java
@@ -0,0 +1,64 @@
+package com.hendisantika.facebook.springbootfacebook.controller;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-facebook
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-13
+ * Time: 08:20
+ */
+
+@Controller
+@RequestMapping("/")
+public class FacebookController {
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public FacebookController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @RequestMapping(value = "feed", method = RequestMethod.GET)
+    public String feed(Model model) {
+
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        User userProfile = facebook.userOperations().getUserProfile();
+        model.addAttribute("userProfile", userProfile);
+        PagedList<Post> userFeed = facebook.feedOperations().getFeed();
+        model.addAttribute("userFeed", userFeed);
+        return "feed";
+    }
+
+    @RequestMapping(value = "friends", method = RequestMethod.GET)
+    public String friends(Model model) {
+
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        User userProfile = facebook.userOperations().getUserProfile();
+        model.addAttribute("userProfile", userProfile);
+        List<User> friends = facebook.friendOperations().getFriendProfiles();
+        model.addAttribute("friends", friends);
+
+        return "friends";
+    }
+}
